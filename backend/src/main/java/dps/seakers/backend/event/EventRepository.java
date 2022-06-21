318b2948dbeaa82028fdc1a2e438db3cf2e806e8
bikemc <32208474+bikemc@users.noreply.github.com>
2022-06-22 01:18:43 +0200

    bugs fixed

--- backend/src/main/java/dps/seakers/backend/event/EventRepository.java
+++ backend/src/main/java/dps/seakers/backend/event/EventRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
-public interface EventRepository extends JpaRepository<EventModel, UUID> {
+public interface EventRepository extends JpaRepository<Event, UUID> {
 
 }
