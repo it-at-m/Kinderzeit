5ff2273343cbf339f1b070fc89241c20a7cddcf9
aashimawadhwa <ashimawadhwa1208@gmail.com>
2022-06-24 00:11:57 +0530

    added picture

--- frontend/components/overview/OverviewEventCard.tsx
+++ frontend/components/overview/OverviewEventCard.tsx
@@ -3,23 +3,8 @@ import React from 'react'
 export default function OverviewEventCard({ event }: { event: Event }) {
     return (
         <div className="relative h-[42rem] w-[24rem] bg-[#F8F9FA] rounded-lg border border-[#BEBBCE] drop-shadow-[0_4px_4px_rgba(0,0,0,0.25)]">
-            <div className="absolute top-0 w-full h-1/4 bg-[#D9D9D9] left-0 right-0 rounded-t-lg flex items-center justify-center">
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="absolute h-[56px] w-[56px]"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="#A2A2A2"
-                    strokeWidth="2"
-                >
-                    <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"
-                    />
-                </svg>
-            </div>
-            <div className="absolute left-0 bottom-0 right-0 h-3/4 rounded-b-lg pl-[17px] flex flex-col">
+            <img className="absolute top-0 w-full h-1/4 left-0 right-0 rounded-t-lg flex items-center justify-center" src="https://images.thrillophilia.com/image/upload/s--90xz2mik--/c_fill,h_775,q_auto,w_1600/f_auto,fl_strip_profile/v1/images/photos/000/334/417/original/1595836118_shutterstock_664589182.jpg.jpg?1595836118"></img>
+            <div className="absolute left-0 bottom-0 right-0 h-3/4 rounded-b-lg pl-[1.8rem] flex flex-col">
                 <h2 className="font-inter font-[600] text-[2rem] leading-[2.4rem] text-[#2B2B2B] w-2/3 line-clamp-2 mt-[1.3rem]">
                     Day Adventure at Sea Life
                 </h2>
