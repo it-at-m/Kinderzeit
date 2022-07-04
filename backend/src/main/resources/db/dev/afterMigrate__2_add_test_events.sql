51e7d62351d85a8e927c64580657c263e5694619
bikemc <32208474+bikemc@users.noreply.github.com>
2022-07-04 13:29:49 +0200

    bug in after_migrate fixed

--- backend/src/main/resources/db/dev/afterMigrate__2_add_test_events.sql
+++ backend/src/main/resources/db/dev/afterMigrate__2_add_test_events.sql
@@ -1,6 +1,5 @@
 INSERT INTO Event
-    (
-    id,
+    (id,
     image_URL,
     event_name,
     event_description,
@@ -20,21 +19,21 @@ INSERT INTO Event
     email_contact)
 VALUES
     (  1,
-        "https://images.thrillophilia.com/image/upload/s--90xz2mik--/c_fill,h_775,q_auto,w_1600/f_auto,fl_strip_profile/v1/images/photos/000/334/417/original/1595836118_shutterstock_664589182.jpg.jpg?1595836118",
-        "Day Adventure at sea",
-        "Discover over 3.000 creatures in more than 33 displays.",
-        "9:00",
+        'xxx',
+		'Sea Life',
+        'Discover over 3.000 creatures in more than 33 displays.',
+        '9:00',
         3,
         14,
         false,
         2,
-        "Sea Life, Moosach",
-        "80335",
+        'Sea Life, Moosach',
+        '80335',
         16.00,
         2,
         2,
         10,
-        2022-06-03,
-        2022-06-04,
-        "info@kunstlabor.org"
+        '2022-06-03',
+        '2022-06-04',
+        'info@kunstlabor.org'
     );
