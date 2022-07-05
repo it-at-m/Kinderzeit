0f94f6072ebc5874c980537c8b9382c3f1424f51
Andrei Bratu <bratuandrei0@gmail.com>
2022-07-05 16:14:44 +0200

    Improving development UX

---
+++ frontend/start.dev.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Running line #3 in this script inside the Dockerfile will
+# not actually open the next.js process. So nginx will work
+# fine but redirect to port 3000 that has nothing.
+# Probably related to layer-style of Docker images.
+# Running them inside the container will fix this.
+pm2 start "yarn dev";
+nginx;
