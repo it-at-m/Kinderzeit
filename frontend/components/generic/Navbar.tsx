3e43db7f9c3158c21259fcba1e1184dedcbc54fc
aashimawadhwa <ashimawadhwa1208@gmail.com>
2022-07-11 18:12:52 +0530

    event details page

--- frontend/components/generic/Navbar.tsx
+++ frontend/components/generic/Navbar.tsx
@@ -1,29 +1,80 @@
+/* eslint-disable @next/next/no-img-element */
+
 import React from 'react'
 
 export default function Navbar() {
     return (
-        <>
-            <header className="bg-pink-300 h-[3rem] my-auto flex justify-center md:justify-start items-center md:pl-2 md:pr-4 relative">
-                <div className="uppercase font-inter font-bold text-[2.5rem]">
-                    Ferienplaner
+        <header>
+            <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5 bg-white border-b-4 gradient-to-r from-sky-500 via-purple-500 to-pink-500">
+                <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
+                    <a href="" className="flex items-center">
+                        <img
+                            className="w-[20rem] h-[2.5rem] mr-6"
+                            src="Logo.png"
+                        ></img>
+                    </a>
+                    <div className="flex items-center lg:order-2">
+                        <a
+                            href="#"
+                            className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800"
+                        >
+                            Log in
+                        </a>
+                        <button
+                            data-collapse-toggle="mobile-menu-2"
+                            type="button"
+                            className="inline-flex items-center p-2 ml-1 text-sm text-black-500 rounded-lg lg:hidden hover:bg-black-100 focus:outline-none focus:ring-2 focus:ring-black-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                            aria-controls="mobile-menu-2"
+                            aria-expanded="false"
+                        >
+                            <span className="sr-only">Open main menu</span>
+                        </button>
+                    </div>
+                    <div
+                        className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1"
+                        id="mobile-menu-2"
+                    >
+                        <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
+                            <li>
+                                <a
+                                    href="#"
+                                    className="block py-2 pr-4 pl-3 text-black rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-black"
+                                    aria-current="page"
+                                >
+                                    Ferienprogramm
+                                </a>
+                            </li>
+                            <li>
+                                <a
+                                    href="#"
+                                    className="block py-2 pr-4 pl-3 text-black rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-black"
+                                    aria-current="page"
+                                >
+                                    Empfehlung
+                                </a>
+                            </li>
+                            <li>
+                                <a
+                                    href="#"
+                                    className="block py-2 pr-4 pl-3 text-black rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-black"
+                                    aria-current="page"
+                                >
+                                    Über Uns
+                                </a>
+                            </li>
+                            <li>
+                                <a
+                                    href="#"
+                                    className="block py-2 pr-4 pl-3 text-sky-700 rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-sky-600"
+                                    aria-current="page"
+                                >
+                                    Event erstellen
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
-                <div className="hidden md:block flex-1" />
-                <div className="hidden md:block font-inter font-[500] text-[1.8rem]">
-                    Ferienprogramm
-                </div>
-                <div className="hidden md:block font-inter font-[500] mx-4 text-[1.8rem]">
-                    Empfehlung
-                </div>
-                <div className="hidden md:block font-inter font-[500] text-[1.8rem]">
-                    Über Uns
-                </div>
-                {/* Mobile Menu */}
-                <div className="absolute right-2 w-8 ml-auto h-full md:hidden flex flex-col items-center justify-center">
-                    <div className="w-full h-[4px] bg-black my-[2px]" />
-                    <div className="w-full h-[4px] bg-black my-[2px]" />
-                    <div className="w-full h-[4px] bg-black my-[2px]" />
-                </div>
-            </header>
-        </>
+            </nav>
+        </header>
     )
 }
--- frontend/components/overview/OverviewEventCard.tsx
+++ frontend/components/overview/OverviewEventCard.tsx
@@ -10,28 +10,102 @@ export default function OverviewEventCard({
 }) {
     return (
         <Link href="/edetails">
-            <div className="max-w-[20rem] bg-[#F8F9FA] rounded-lg border-[#BEBBCE] drop-shadow-[0_4px_4px_rgba(0,0,0,0.25)] flex flex-col">
-                <img
-                    className="top-0 w-full h-32 left-0 right-0 rounded-t-lg flex items-center justify-center"
-                    src=""
-                />
-                <div className="flex-1 rounded-b-lg flex flex-col pl-2 pb-2">
-                    <h2 className="font-inter font-[600] text-[1rem] text-[#2B2B2B] line-clamp-2">
-                        {event.event_name}
-                    </h2>
-                    <div className="flex-wrap grid grid-cols-2 gap-4 place-content-start mt-2 pl-2">
-                        <div>Kosten</div>
-                        <div>{event.price}</div>
-                        <div>Alter</div>
-                        <div>
-                            {event.min_age} - {event.max_age}
+            <div className="w-full h-full py-5 flex justify-center items-center">
+                <div className="relative pl-1  flex justify-center rounded-xl hover:scale-105 duration-500 transform transition cursor-pointer">
+                    <div className="w-50 pb-2 bg-white rounded-xl shadow-xl z-10">
+                        <div className="relative pt-4">
+                            <a className="flex justify-center items-center top-80 right-0 mb-2 mr-4 px-2 absolute rounded-lg bg-yellow-500 text-white text-s font-small">
+                                {event.price}
+                                <p>&nbsp;€&nbsp;p.&nbsp;P.</p>
+                            </a>
                         </div>
-                        <div>Uhrzeit</div>
-                        <div>{event.start_time}</div>
-                        <div>Kosten</div>
-                        <div>{event.price}</div>
-                        <div>Kontakt</div>
-                        <div>{event.email_contact}</div>
+                        <div className="relative">
+                            {/*  card image */}
+                            <img
+                                src="https://www.kindercare.com/-/media/contenthub/images/article-images/activities%20for%20kids/arts%20and%20crafts/painting-with-nature/colorful-paint-splatters-compressor.jpg?la=en&hash=DA9C3A0777DDA8124A30C22CC2D73C508D758F49"
+                                className="max-h-60 object-cover rounded-t-xl"
+                                alt=""
+                            />
+                        </div>
+                        <div className="px-2 py-1 ">
+                            {/* <!-- Product Title --> */}
+                            <div className="text-xl px-2">
+                                {event.event_name}
+                            </div>
+                            <div className="flex py-2">
+                                <div className="flex justify-between item-center"></div>
+                            </div>
+                            {/* card details */}
+                            <div className="p-3">
+                                <div className="pt-6">
+                                    <p className="font-200 text-sm text-gray-400">
+                                        Datum and Uhrzeit
+                                    </p>
+                                    <div className="flex space-x-12">
+                                        <span className="font-semibold text-lg leading-6 text-gray-700 my-2">
+                                            {event.begin_date}
+                                        </span>
+                                        <span className="font-semibold text-lg leading-6 text-gray-700 my-2">
+                                            {event.start_time}
+                                        </span>
+                                    </div>
+                                </div>
+                                <div className="pt-2">
+                                    <p className="font-200 text-sm text-gray-400">
+                                        Alter
+                                    </p>
+                                    <div className="flex space-x-2">
+                                        <span className="font-semibold text-lg leading-6 text-gray-700 my-2">
+                                            {event.min_age}
+                                        </span>
+                                        <span className="font-semibold text-lg leading-6 text-gray-700 my-2">
+                                            -
+                                        </span>
+                                        <span className="font-semibold text-lg leading-6 text-gray-700 my-2">
+                                            {event.max_age}
+                                        </span>
+                                        <span className="font-semibold text-lg leading-6 text-gray-700 my-2">
+                                            Jahre
+                                        </span>
+                                    </div>
+                                </div>
+                                <div className="pt-2">
+                                    <p className="font-200 text-sm text-gray-400">
+                                        Verantstaltungsort
+                                    </p>
+                                    <div className="flex space-x-12">
+                                        <span className="font-semibold text-lg leading-6 text-gray-700 my-2">
+                                            {event.event_address}
+                                        </span>
+                                        <span className="font-semibold text-sm leading-6 text-gray-700 my-2">
+                                            {}
+                                        </span>
+                                    </div>
+                                </div>
+                                <div className="p-8">
+                                    <button className=" bg-rose-200 rounded-md p-2 text-red-600">
+                                        Nur noch {event.places_available} Plätze
+                                        frei!
+                                    </button>
+                                </div>
+
+                                {/* card bottom */}
+                                <div className="flex space-x-4">
+                                    <a
+                                        className=" py-1 px-5 text-gray-500 underline decoration-1"
+                                        href="#"
+                                    >
+                                        mehr info
+                                    </a>
+                                    <a
+                                        className="px-4 py-1 bg-green-700 text-gray-100 rounded-full hover:bg-green-900"
+                                        href="#"
+                                    >
+                                        Jetzt buchen
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
